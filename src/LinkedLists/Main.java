f106c09a6e918a0ab695edbb1d08c26706b2c7b8
ELIJAH.AYEETA <elijate@gmail.com>
2023-08-22 04:05:35 +0300

    linked list

--- src/LinkedLists/Main.java
+++ src/LinkedLists/Main.java
@@ -3,5 +3,14 @@ package LinkedLists;
 public class Main {
     //Difference between an arraylist and LinkedList
     //Arraylist uses indexes while linkedlist uses pointers
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+    }
 }
